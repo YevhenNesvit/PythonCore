28518593ac6261d6095e2a6afc35a333a17895c5
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-10-12 17:29:55 +0300

    recursion

---
+++ PythonCoreHw07-17.py
@@ -0,0 +1,31 @@
+def encode(data):
+    res = list()
+    global quantity
+    if not data :
+        return []
+    def iflist(data) :
+        quantity = 0
+        for i in range(len(data)) :
+            if i != len(data) - 1:
+                if data[i] == data[i + 1] :
+                    quantity += 1
+                else :
+                    break
+            else :
+                quantity = len(data) - 1
+                break
+        return quantity
+
+    if isinstance(data, list) :
+        quantity = iflist(data)
+    else :
+        data = list(data)
+        quantity = iflist(data)
+    res.append(data[quantity])
+    res.append(quantity + 1)
+    data = data[quantity + 1:]
+    result = res
+    result.extend(encode(data))
+    
+    return result
+print(encode("XXXZZXXYYYZZ"))
